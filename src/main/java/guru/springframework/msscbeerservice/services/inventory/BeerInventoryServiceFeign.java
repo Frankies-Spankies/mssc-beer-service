463635b6f0af1e6af3c086f18ca4c2cc45be4cb0
Francisco Gamez <franciscogamezdg@gmail.com>
2020-10-11 17:16:52 -0500

    Feing client para getOnhandInventory

---
+++ src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceFeign.java
@@ -0,0 +1,37 @@
+package guru.springframework.msscbeerservice.services.inventory;
+
+import guru.springframework.msscbeerservice.services.inventory.model.BeerInventoryDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Profile("local-discovery")
+@Service
+public class BeerInventoryServiceFeign implements BeerInventoryService {
+
+    private final InventoryServiceFeingClient inventoryServiceFeingClient;
+
+    @Override
+    public Integer getOnhandInventory(UUID beerId) {
+        log.debug("Calling Inventory Service from Feign client ...");
+
+        ResponseEntity<List<BeerInventoryDto>> responseEntity = inventoryServiceFeingClient.getOnhandInventory(beerId);
+
+        Integer onHand = Objects.requireNonNull(responseEntity.getBody())
+                .stream()
+                .mapToInt(BeerInventoryDto::getQuantityOnHand)
+                .sum();
+
+        return onHand;
+    }
+}
